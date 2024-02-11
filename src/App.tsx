4edd21e31ecca3296b19878529798b52df412afe
Can Gürel <91556629+ahmetcangurel@users.noreply.github.com>
2024-02-11 12:10:45 +0300

    Add => bg image and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 function App() {
-  return <div>cangurel.dev</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        backgroundImage: `url(${require("./assets/images/background.jpeg")})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "rgba(0, 0, 0, 0.8)",
+          position: "absolute",
+          height: "100%",
+          width: "100%",
+          top: 0,
+          left: 0,
+        }}
+      />
+      <h1 style={{ color: "white", zIndex: 1, fontFamily: "monospace" }}>
+        cangurel.dev
+      </h1>
+    </div>
+  );
 }
 
 export default App;
